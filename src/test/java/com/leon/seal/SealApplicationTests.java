091a2cf267371cfd0a46a8160b58dae05d40c341
QLeon <quanvo.hello@gmail.com>
2025-04-04 15:20:01 +0700

    init

---
+++ src/test/java/com/leon/seal/SealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leon.seal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
